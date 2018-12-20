fffefd2c1b0f97adbed55e831e8f026c7c0658cf
Sergey Rodin <rodin.s@rambler.ru>
2018-12-20 14:41:00 +0200

    Add gui elements

---
+++ gui.py
@@ -0,0 +1,99 @@
+# -*- coding: UTF-8 -*-
+# Gui
+import pygtk
+pygtk.require('2.0')
+import gtk
+
+class Gui(gtk.Window):
+    def __init__(self):
+        super(Gui, self).__init__()
+        
+        self.SPINNER_SIZE = 32
+        self.connect("destroy", self.on_destroy)
+        self.set_border_width(5)
+        self.set_size_request(780, 400)
+        #TODO: set application image
+        self.set_title("Cities info")
+
+        self.sp_load_db = gtk.Spinner()
+        self.sp_load_db.set_size_request(self.SPINNER_SIZE, self.SPINNER_SIZE)
+
+        countries_store = gtk.ListStore(gtk.gdk.Pixbuf, str, int)
+        tv_countries = self.create_tree_view()
+        tv_countries.set_model(countries_store)
+        sw_countries = self.create_scrolled_window()
+        sw_countries.add(tv_countries)
+        self.fr_countries = gtk.Frame("Countries")
+        self.fr_countries.add(sw_countries)
+        self.fr_countries.show_all()
+
+        cities_store = gtk.ListStore(gtk.gdk.Pixbuf, str)
+        tv_cities = self.create_tree_view()
+        tv_cities.set_model(cities_store)
+        sw_cities = self.create_scrolled_window()
+        sw_cities.add(tv_cities)
+        self.fr_cities = gtk.Frame("Cities")
+        self.fr_cities.add(sw_cities)
+        self.fr_cities.show_all()
+        
+        btn_load_db_error = gtk.Button("Retry")
+        btn_load_db_error.connect("clicked", self.btn_load_db_error_clicked)
+        btn_load_db_error.show()
+        self.vb_load_db_error = gtk.VBox(False, 1)
+        self.vb_load_db_error.pack_start(btn_load_db_error, True, False, 10)
+        
+        hbox = gtk.HBox(False, 5)
+        hbox.pack_start(self.sp_load_db, True, False, 1)
+        hbox.pack_start(self.fr_countries, True, True, 5)
+        hbox.pack_start(self.fr_cities, True, True, 5)
+        hbox.pack_start(self.vb_load_db_error, True, False, 1)
+
+        self.add(hbox)
+        hbox.show()
+        self.show()
+
+    def create_tree_view(self):
+        tree_view = gtk.TreeView()
+    
+        renderer_pixbuf = gtk.CellRendererPixbuf()
+        column = gtk.TreeViewColumn("Image", renderer_pixbuf, pixbuf=0)
+        tree_view.append_column(column)
+        
+        renderer_text = gtk.CellRendererText()
+        column = gtk.TreeViewColumn("Title", renderer_text, text=1)
+        tree_view.append_column(column)
+        
+        tree_view.set_headers_visible(False)
+        
+        return tree_view
+
+    def on_destroy(self, widget):
+        gtk.main_quit()
+        
+    def btn_load_db_error_clicked(self, widget):
+        print "Retry load db"
+
+    def create_scrolled_window(self):
+        scrolled_window = gtk.ScrolledWindow()
+        scrolled_window.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        scrolled_window.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+        return scrolled_window
+
+    def show_loading_indicator(self):
+        self.sp_load_db.show()
+        self.sp_load_db.start()
+        self.fr_countries.hide()
+        self.fr_cities.hide()
+        self.vb_load_db_error.hide()
+
+    def show_data(self):
+        self.sp_load_db.hide()
+        self.sp_load_db.stop()
+        self.fr_countries.show()
+        self.fr_cities.show()
+
+    def show_error(self):
+        self.sp_load_db.hide()
+        self.sp_load_db.stop()
+        self.vb_load_db_error.show()
+    
--- py2_cities_task.py
+++ py2_cities_task.py
@@ -4,49 +4,62 @@ import urllib2
 import json
 import sqlite3
 
-conn = sqlite3.connect('cities.db')
-cur = conn.cursor()
+import gtk
+import gobject
 
-def print_countries():
-    for country_id, country in cur.execute('SELECT * FROM countries'):
-        print country
+from gui import Gui
+
+##conn = sqlite3.connect('cities.db')
+##cur = conn.cursor()
+##
+##def print_countries():
+##    for country_id, country in cur.execute('SELECT * FROM countries'):
+##        print country
+##
+##
+##def print_cities(country_id):
+##    for city, in cur.execute('SELECT city FROM cities WHERE country_id=?',
+##                             (country_id,)):
+##        print city
+##        
+##def load_db():
+##    cur.execute('CREATE TABLE countries (id INTEGER PRIMARY KEY, country TEXT) WITHOUT ROWID')
+##    cur.execute('CREATE TABLE cities (city TEXT, country_id INTEGER, FOREIGN KEY(country_id) REFERENCES countries(id))')
+##
+##    link = "https://raw.githubusercontent.com/David-Haim/CountriesToCitiesJSON/master/countriesToCities.json"
+##    try:
+##        print "Loading data into database..."
+##        response = urllib2.urlopen(link)
+##        js = json.load(response)
+##        country_id = 1;
+##        for country in js:
+##            cur.execute('INSERT INTO countries VALUES (?, ?)',
+##                        (country_id, country))
+##            for city in js[country]:
+##                cur.execute('INSERT INTO cities VALUES(?, ?)',
+##                            (city, country_id))
+##            country_id += 1
+##        conn.commit()
+##    except Exception as ex:
+##        print ex
+##
+##try:
+##    print_countries()
+##    print_cities(93)
+##except sqlite3.OperationalError as ex:
+##    print ex
+##    load_db()
+##    print_db()
+##conn.close()
 
 
-def print_cities(country_id):
-    for city, in cur.execute('SELECT city FROM cities WHERE country_id=?',
-                             (country_id,)):
-        print city
-        
-def load_db():
-    cur.execute('CREATE TABLE countries (id INTEGER PRIMARY KEY, country TEXT) WITHOUT ROWID')
-    cur.execute('CREATE TABLE cities (city TEXT, country_id INTEGER, FOREIGN KEY(country_id) REFERENCES countries(id))')
+def main():
+    gobject.threads_init()
+    Gui()
+    gtk.main()
 
-    link = "https://raw.githubusercontent.com/David-Haim/CountriesToCitiesJSON/master/countriesToCities.json"
-    try:
-        print "Loading data into database..."
-        response = urllib2.urlopen(link)
-        js = json.load(response)
-        country_id = 1;
-        for country in js:
-            cur.execute('INSERT INTO countries VALUES (?, ?)',
-                        (country_id, country))
-            for city in js[country]:
-                cur.execute('INSERT INTO cities VALUES(?, ?)',
-                            (city, country_id))
-            country_id += 1
-        conn.commit()
-    except Exception as ex:
-        print ex
-
-try:
-    print_countries()
-    print_cities(93)
-except sqlite3.OperationalError as ex:
-    print ex
-    load_db()
-    print_db()
-conn.close()
-
+if __name__ == "__main__":
+    main()
 
     
     
