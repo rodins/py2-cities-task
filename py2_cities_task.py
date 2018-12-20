97dc7b61701ddc2b6d56bf515e155fd011ae8887
Sergey Rodin <rodin.s@rambler.ru>
2018-12-20 07:32:05 +0200

    Create db, load to db. Print countries, print cities of country

--- py2_cities_task.py
+++ py2_cities_task.py
@@ -2,16 +2,53 @@
 # py2_cities_task.py
 import urllib2
 import json
+import sqlite3
 
-link = "https://raw.githubusercontent.com/David-Haim/CountriesToCitiesJSON/master/countriesToCities.json"
-try:
-    response = urllib2.urlopen(link)
-    js = json.load(response)
-    for country in js:
+conn = sqlite3.connect('cities.db')
+cur = conn.cursor()
+
+def print_countries():
+    for country_id, country in cur.execute('SELECT * FROM countries'):
         print country
-        for city in js[country]:
-            print '\t' + city
-except Exception as ex:
+
+
+def print_cities(country_id):
+    for city, in cur.execute('SELECT city FROM cities WHERE country_id=?',
+                             (country_id,)):
+        print city
+        
+def load_db():
+    cur.execute('CREATE TABLE countries (id INTEGER PRIMARY KEY, country TEXT) WITHOUT ROWID')
+    cur.execute('CREATE TABLE cities (city TEXT, country_id INTEGER, FOREIGN KEY(country_id) REFERENCES countries(id))')
+
+    link = "https://raw.githubusercontent.com/David-Haim/CountriesToCitiesJSON/master/countriesToCities.json"
+    try:
+        print "Loading data into database..."
+        response = urllib2.urlopen(link)
+        js = json.load(response)
+        country_id = 1;
+        for country in js:
+            cur.execute('INSERT INTO countries VALUES (?, ?)',
+                        (country_id, country))
+            for city in js[country]:
+                cur.execute('INSERT INTO cities VALUES(?, ?)',
+                            (city, country_id))
+            country_id += 1
+        conn.commit()
+    except Exception as ex:
+        print ex
+
+try:
+    print_countries()
+    print_cities(93)
+except sqlite3.OperationalError as ex:
     print ex
+    load_db()
+    print_db()
+conn.close()
+
+
+    
+    
     
     
